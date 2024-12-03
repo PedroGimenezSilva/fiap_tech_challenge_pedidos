f677066f79773db2285cdcd0a9caacbf1a33ae57
Victor Leite <victor_leite@outlook.com.br>
2024-12-03 00:19:31 -0300

    first commit

---
+++ src/main/java/com/techchallenge/fiap_pedidos/FiapPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.techchallenge.fiap_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techchallenge/fiap_pedidos/FiapPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.fiap_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
